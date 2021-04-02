b11110ac6576a5156f2a4e05aac165005e40cf43
SangheonYi <tmzpdpdlwl@gmail.com>
2021-04-02 21:18:01 +0900

    fix: Remove test code

--- app/src/main/java/com/sayi/sayiocr/OCR.kt
+++ app/src/main/java/com/sayi/sayiocr/OCR.kt
@@ -25,7 +25,6 @@ class OCR(inMain: MainActivity)  // 초기화 작업
     private lateinit var vibrator: Vibrator
     var image: Bitmap? = null //갤러리에서 이미지 받아와
 
-    @kotlin.time.ExperimentalTime
     @Synchronized
     override fun run() {
         for (f in model.folderMetaList) {
@@ -42,40 +41,22 @@ class OCR(inMain: MainActivity)  // 초기화 작업
         main.terminateService()
     }
 
-    @kotlin.time.ExperimentalTime
     private fun ocrTrans(folder: FolderMeta) {
-        val remover = FileWriter("${main.filesDir}/test.txt")
-        val bufferRemover = BufferedWriter(remover)
-        bufferRemover.write("")
-        bufferRemover.close()
         while (folder.page < folder.uriList.size) {
-            val ocrDuration = measureTime{
-
             image = getCapturedImage(folder.uriList[folder.page])
             model.sTess!!.setImage(image)
             model.ocrIndex++
             folder.page++
             transResult = model.sTess!!.utF8Text
             model.bigText.addSentence(transResult)
+            //변환 과정
             if (model.ocrIndex < folder.uriList.size)
-                main.mHandler.sendMessage(Message.obtain(main.mHandler, model.VIEW_PROGRESS_ING)) //변환 과정
-            main.mHandler.sendMessage(Message.obtain(main.mHandler, model.VIEW_RESULT_SET, transResult)) //결과 화면 set
-            if (model.state == "playing") main.mHandler.sendMessage(Message.obtain(main.mHandler, model.VIEW_READ_HIGHLIGHT)) //읽는 중일 시 강조
-            }
-            val batteryStatus: Intent? = IntentFilter(Intent.ACTION_BATTERY_CHANGED).let { ifilter ->
-                main.registerReceiver(null, ifilter)
-            }
-            // 배터리 퍼센트를 알려주는 코드입니다.(소수점으로 표시됩니다.)
-            val batteryPct: Float? = batteryStatus?.let { intent ->
-                val level: Int = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1)
-                val scale: Int = intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1)
-                level / scale.toFloat()
-            }
-            val fileWriter = FileWriter("${main.filesDir}/test.txt", true)
-            val bufferWriter = BufferedWriter(fileWriter)
-            bufferWriter.write("${model.ocrIndex} took ${ocrDuration.inSeconds.roundToInt()} sec," +
-                    " remain battery: ${batteryPct}\n")
-            bufferWriter.close()
+                main.mHandler.sendMessage(Message.obtain(main.mHandler, model.VIEW_PROGRESS_ING))
+            //결과 화면 set
+            main.mHandler.sendMessage(Message.obtain(main.mHandler, model.VIEW_RESULT_SET, transResult))
+            //읽는 중일 시 강조
+            if (model.state == "playing")
+                main.mHandler.sendMessage(Message.obtain(main.mHandler, model.VIEW_READ_HIGHLIGHT))
         }
     }
 
